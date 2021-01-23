4309e250983f06dc8b7d7273aaf9fa56feeae0c4
Marvin T <contact@marvinkturner.com>
2021-01-22 23:58:37 -0500

    feat: render review component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Review from './Review';
+function App() {
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+}
+
+export default App;
